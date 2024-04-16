3bd56eb5e261e4fa41d57636fafefd6a55773418
Hristo Boyadzhiev <hristo_boyadzhiev@abv.bg>
2024-04-16 18:01:45 +0300

    add guard in winner page

--- tic-tac-toe/src/components/Winner/Winner.js
+++ tic-tac-toe/src/components/Winner/Winner.js
@@ -1,10 +1,21 @@
+import { useEffect } from 'react'
 import { useGameContext } from '../Context/GameContext'
 import styles from './Winner.module.css'
+import { useNavigate } from 'react-router-dom'
 
 export function Winner(props){
     const {winner} = useGameContext()
+    const navigate = useNavigate()
+
+    useEffect(()=>{
+        if(!winner){
+            navigate('/')
+        }
+    }, [winner])
 
     return (
-        <h1 className={styles.winner}>Winner is {winner}</h1>
+        <section className={styles.board}>
+        <h1 className={styles.winner}>Winner is {winner}</h1> 
+        </section>
     )
 }
